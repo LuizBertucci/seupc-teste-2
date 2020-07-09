df1944bf6961d706d294f1005cad4e708d9ffb91
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-09 16:16:19 -0300

    criando o route e o controller dos notebooks

---
+++ app/controllers/notebooks_controller.rb
@@ -0,0 +1,64 @@
+class NotebooksController < ApplicationController
+
+  before_action :find_notebook, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @notebooks = Notebook.all
+    if params[:search]
+      if params[:search][:query]
+        @notebookresult = Notebook.find_by(name: params[:search][:query])
+        if @notebookresult
+          redirect_to notebook_path(@notebookresult)
+        else
+          # redirect_to action:'index', alert: "notebook not found"
+          # flash.alert
+          flash[:error] = 'notebook not found'
+          redirect_to action:'index', danger: "notebook not found"
+        end
+      end
+    end
+  end
+
+  def show
+
+  end
+
+  def new
+    @notebook = Notebook.new
+  end
+
+  def create
+    @notebook = Notebook.new(notebooks_params)
+    if @notebook.save
+      redirect_to notebook_path(@notebook)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @notebook.update(notebooks_params)
+
+    redirect_to notebook_path
+  end
+
+  private
+
+  def find_notebook
+    @notebook = notebook.find(params[:id])
+  end
+
+  def notebooks_params
+    params.require(:notebook).permit(:bar_code, :full_price, :offer_price,
+      :brand, :modelo, :processor, :color,
+      :screen, :ram, :hd, :ssd, :placa_video, :keyboard,
+      :amazon_sales_rank, :guarantee)
+  end
+end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  resources :notebooks, only: [:index, :new, :show, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'notebooks#index'
+
+
 end
