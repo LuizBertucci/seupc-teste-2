2861c0c1e4ea98674902889b1da15d75663757f5
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-11 07:46:36 -0300

    adicionando o link da amazon na seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,101 +29,102 @@ Notebook.destroy_all
 
 puts 'database cleanned'
 
-# Lógica para fazer um request http manual para a api da amazon
+# # Lógica para fazer um request http manual para a api da amazon
 
-request = Vacuum.new(marketplace: 'BR',
-                     access_key: 'AKIAJDG2EY7UPZXAW4TA',
-                     secret_key: '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',
-                     partner_tag: 'seupc01-20')
-
-
-
-# Loop de 10 vezes para popular o DB, cada request só retornam 10 notebooks.
-i = 1
-while i < 5 do
-  sleep(5) # garantir que não vamos floodar os requests e ser bloqueado por algumas horas pela amazon.
-  response = request.search_items(keywords:'notebook', search_index:'Computers',
-  item_page: "#{i}".to_i, sort_by:'AvgCustomerReviews', resources:[
-   "ItemInfo.ExternalIds", "ItemInfo.ProductInfo",
-   "ItemInfo.Features", "ItemInfo.Title",
-   "BrowseNodeInfo.WebsiteSalesRank","ItemInfo.ManufactureInfo",
-   "ItemInfo.ByLineInfo", "Images.Primary.Medium",
-   "Offers.Summaries.HighestPrice", "Offers.Summaries.LowestPrice"
-  ])
-
-
-  # Parseando resposta da API.
+# request = Vacuum.new(marketplace: 'BR',
+#                      access_key: 'AKIAJDG2EY7UPZXAW4TA',
+#                      secret_key: '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',
+#                      partner_tag: 'seupc01-20')
 
 
 
-
-  response = response.to_h
-
-  # puts response
-
-  File.open("db/temporaryfiles/temp_rating_#{i}.json", 'wb') do |file|
-    file.write(JSON.generate(response))
-  end
-
-  puts "página #{i} gerada"
-  i += 1
-end
-
-
-
-
-
-# # Parseando meu temporary.json gerado pela api da Amazon
+# # Loop de 10 vezes para popular o DB, cada request só retornam 10 notebooks.
 # i = 1
+# while i < 5 do
+#   sleep(5) # garantir que não vamos floodar os requests e ser bloqueado por algumas horas pela amazon.
+#   response = request.search_items(keywords:'notebook', search_index:'Computers',
+#   item_page: "#{i}".to_i, sort_by:'AvgCustomerReviews', resources:[
+#    "ItemInfo.ExternalIds", "ItemInfo.ProductInfo",
+#    "ItemInfo.Features", "ItemInfo.Title",
+#    "BrowseNodeInfo.WebsiteSalesRank","ItemInfo.ManufactureInfo",
+#    "ItemInfo.ByLineInfo", "Images.Primary.Medium",
+#    "Offers.Summaries.HighestPrice", "Offers.Summaries.LowestPrice"
+#   ])
 
-# while i < 11 do
 
-#   puts "lendo temp#{i}.json"
-
-#   filepath = "db/temporaryfiles/temp#{i}.json"
-
-#   response = File.read(filepath)
-
-#   response_file = eval(response)
-
-#   # lógica para ler o primeiro response file com 10 itens, gerando novos 10 itens para o nosso DB
-
-#   notebooks = response_file[:SearchResult][:Items]
-
-#   notebooks.each do |notebook|
-
-#     laptop = Notebook.create!(
-#       bar_code:(notebook[:ItemInfo][:ExternalIds][:EANs][:DisplayValues][0] if notebook[:ItemInfo][:ExternalIds]),
-#       full_price: notebook[:Offers][:Summaries][0][:HighestPrice][:DisplayAmount],
-#       offer_price:notebook[:Offers][:Summaries][0][:LowestPrice][:DisplayAmount],
-#       brand:notebook[:ItemInfo][:ByLineInfo][:Brand][:DisplayValue],
-#       modelo:(notebook[:ItemInfo][:ManufactureInfo][:ItemPartNumber][:DisplayValue] if notebook[:ItemInfo][:ManufactureInfo] && notebook[:ItemInfo][:ManufactureInfo][:ItemPartNumber]),
-#       processor:( notebook[:ItemInfo][:ProductInfo] && notebook[:ItemInfo][:ProductInfo][:Size] ? notebook[:ItemInfo][:ProductInfo][:Size][:DisplayValue] : notebook[:ItemInfo][:Title][:DisplayValue]),
-#       color:(notebook[:ItemInfo][:ProductInfo][:Color][:DisplayValue] if notebook[:ItemInfo][:ProductInfo] && notebook[:ItemInfo][:ProductInfo][:Color]),
-#       screen:notebook[:ItemInfo][:Title][:DisplayValue],
-#       weight:( notebook[:ItemInfo][:ProductInfo] && notebook[:ItemInfo][:ProductInfo][:ItemDimensions] && notebook[:ItemInfo][:ProductInfo][:ItemDimensions][:Weight]? notebook[:ItemInfo][:ProductInfo][:ItemDimensions][:Weight][:DisplayValue] : notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]), # Tá em libras
-#       ram:notebook[:ItemInfo][:Title][:DisplayValue],
-#       hd:(notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]),
-#       ssd:(notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]),
-#       placa_video:(notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]),
-#       keyboard:(notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]),
-#       amazon_sales_rank:(notebook[:BrowseNodeInfo][:WebsiteSalesRank][:SalesRank] if notebook[:BrowseNodeInfo]) ,
-#       guarantee:(notebook[:ItemInfo][:ManufactureInfo][:Warranty][:DisplayValue] if notebook[:ItemInfo][:ManufactureInfo] && notebook[:ItemInfo][:ManufactureInfo][:Warranty] )
-#    )
-
-#     # adicionando a foto a cada notebook
-#     file= open(notebook[:Images][:Primary][:Medium][:URL])
-#     laptop.photo.attach(io:file, filename: "randomavatar.jpg")
+#   # Parseando resposta da API.
 
 
 
 
-#     puts '+ 1 notebook added to our DB'
+#   response = response.to_h
+
+#   # puts response
+
+#   File.open("db/temporaryfiles/temp_rating_#{i}.json", 'wb') do |file|
+#     file.write(JSON.generate(response))
 #   end
-#    puts 'notebooks sucessfully added to your database'
 
+#   puts "página #{i} gerada"
 #   i += 1
 # end
+
+
+
+
+
+# Parseando meu temporary.json gerado pela api da Amazon
+i = 1
+
+while i < 11 do
+
+  puts "lendo temp#{i}.json"
+
+  filepath = "db/temporaryfiles/temp#{i}.json"
+
+  response = File.read(filepath)
+
+  response_file = eval(response)
+
+  # lógica para ler o primeiro response file com 10 itens, gerando novos 10 itens para o nosso DB
+
+  notebooks = response_file[:SearchResult][:Items]
+
+  notebooks.each do |notebook|
+
+    laptop = Notebook.create!(
+      bar_code:(notebook[:ItemInfo][:ExternalIds][:EANs][:DisplayValues][0] if notebook[:ItemInfo][:ExternalIds]),
+      full_price: notebook[:Offers][:Summaries][0][:HighestPrice][:DisplayAmount],
+      offer_price:notebook[:Offers][:Summaries][0][:LowestPrice][:DisplayAmount],
+      brand:notebook[:ItemInfo][:ByLineInfo][:Brand][:DisplayValue],
+      modelo:(notebook[:ItemInfo][:ManufactureInfo][:ItemPartNumber][:DisplayValue] if notebook[:ItemInfo][:ManufactureInfo] && notebook[:ItemInfo][:ManufactureInfo][:ItemPartNumber]),
+      processor:( notebook[:ItemInfo][:ProductInfo] && notebook[:ItemInfo][:ProductInfo][:Size] ? notebook[:ItemInfo][:ProductInfo][:Size][:DisplayValue] : notebook[:ItemInfo][:Title][:DisplayValue]),
+      color:(notebook[:ItemInfo][:ProductInfo][:Color][:DisplayValue] if notebook[:ItemInfo][:ProductInfo] && notebook[:ItemInfo][:ProductInfo][:Color]),
+      screen:notebook[:ItemInfo][:Title][:DisplayValue],
+      weight:( notebook[:ItemInfo][:ProductInfo] && notebook[:ItemInfo][:ProductInfo][:ItemDimensions] && notebook[:ItemInfo][:ProductInfo][:ItemDimensions][:Weight]? notebook[:ItemInfo][:ProductInfo][:ItemDimensions][:Weight][:DisplayValue] : notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]), # Tá em libras
+      ram:notebook[:ItemInfo][:Title][:DisplayValue],
+      hd:(notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]),
+      ssd:(notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]),
+      placa_video:(notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]),
+      keyboard:(notebook[:ItemInfo][:Features][:DisplayValues] if notebook[:ItemInfo][:Features]),
+      amazon_sales_rank:(notebook[:BrowseNodeInfo][:WebsiteSalesRank][:SalesRank] if notebook[:BrowseNodeInfo]) ,
+      guarantee:(notebook[:ItemInfo][:ManufactureInfo][:Warranty][:DisplayValue] if notebook[:ItemInfo][:ManufactureInfo] && notebook[:ItemInfo][:ManufactureInfo][:Warranty] ),
+      link_amazon:notebook[:DetailPageURL]
+   )
+
+    # adicionando a foto a cada notebook
+    file= open(notebook[:Images][:Primary][:Medium][:URL])
+    laptop.photo.attach(io:file, filename: "randomavatar.jpg")
+
+
+
+
+    puts '+ 1 notebook added to our DB'
+  end
+   puts 'notebooks sucessfully added to your database'
+
+  i += 1
+end
 
 
 
