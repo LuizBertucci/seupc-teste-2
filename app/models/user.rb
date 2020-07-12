a4a27a30011d61556a0c309d6850579bed821bf1
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-12 09:07:36 -0300

    setando como default o usuário não ser admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, length: { minimum: 3 }
+
+  after_initialize :init
+
+  def init
+    self.admin ||= false
+  end
+
 end
