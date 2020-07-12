386a397c3128e6c1b6444b2d65069c5672095825
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-12 10:38:44 -0300

    adicionando a lógica para dispay correto no show somente pra admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :notebooks, only: [:index, :new, :show, :create, :edit, :update]
+  resources :notebooks, only: [:index, :new, :show, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'notebooks#index'
 
