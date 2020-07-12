a37c413d994e4bc06c74a9a49ce5fc7b25bbbad6
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-12 15:55:28 -0300

    fazendo a lógica para toda vez que você editar um notebook o card dele ficará verde

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -3,4 +3,12 @@ class Notebook < ApplicationRecord
   has_one_attached :photo
 
   validates :asin, presence: true, uniqueness: true
+
+
+  after_initialize :init
+
+  def init
+    self.edited ||= false
+  end
+
 end
