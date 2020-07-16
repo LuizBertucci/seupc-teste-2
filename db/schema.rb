1f6a9c547d26cabf4259397f40473ba76dbf0b65
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-16 07:12:45 -0300

    adicionando nome à tabela de notebooks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_174729) do
+ActiveRecord::Schema.define(version: 2020_07_16_101042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_07_15_174729) do
     t.string "link_magalu"
     t.string "link_submarino"
     t.string "link_americanas"
+    t.string "name"
   end
 
   create_table "saved_notebooks", force: :cascade do |t|
