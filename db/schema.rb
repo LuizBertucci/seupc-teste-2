27247410d1f9d762cb1862d2bb86224e3add4459
matheus moreira ferreira <moreira.mferreira@gmail.com>
2020-07-19 18:35:41 -0300

    Screen width

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_195926) do
+ActiveRecord::Schema.define(version: 2020_07_19_213430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_195926) do
     t.string "americanas_price"
     t.string "submarino_price"
     t.string "screen_type"
+    t.string "screen_width"
   end
 
   create_table "saved_notebooks", force: :cascade do |t|
