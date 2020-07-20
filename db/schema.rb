de73c5ffff9359b7864c4acea4070c4415acc032
matheus moreira ferreira <moreira.mferreira@gmail.com>
2020-07-19 22:17:29 -0300

    screen_type now is screen_quality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_213430) do
+ActiveRecord::Schema.define(version: 2020_07_20_011120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_213430) do
     t.string "magalu_price"
     t.string "americanas_price"
     t.string "submarino_price"
-    t.string "screen_type"
+    t.string "screen_quality"
     t.string "screen_width"
   end
 
