7f9630531dd36ca8937dde7b67df1858c2ba61e0
Yuri Gravatá <ygravata@gmail.com>
2020-07-28 20:14:49 -0300

    adding acts_as_list config

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,11 +1,10 @@
 class Notebook < ApplicationRecord
 
   has_one_attached :photo
-
   validates :asin, presence: true, uniqueness: true
-
-
   after_initialize :init
+  
+  acts_as_list column: :position, add_new_at: :bottom
 
   def init
     self.edited ||= false
